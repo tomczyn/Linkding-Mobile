e0c2894305b16530d129413f079ff5824de3f6fb
tomczyn <dev@tomczyn.com>
2023-03-22 17:01:00 +0100

    Improved styling on androidApp

--- androidApp/src/main/java/com/tomczyn/linkding/android/ui/login/LoginScreen.kt
+++ androidApp/src/main/java/com/tomczyn/linkding/android/ui/login/LoginScreen.kt
@@ -1,9 +1,11 @@
 package com.tomczyn.linkding.android.ui.login
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,8 +17,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.tomczyn.linkding.android.ui.common.AppScreen
 import com.tomczyn.linkding.android.ui.common.theme.Dimens
@@ -25,30 +29,35 @@ import com.tomczyn.linkding.android.ui.common.theme.Dimens
 fun LoginRoute() {
     LoginScreen(
         demoClick = { /*TODO*/ },
-        loginClick = { host, username, password -> /*TODO*/ },
+        loginClick = { _, _ -> /*TODO*/ },
     )
 }
 
 @Composable
 fun LoginScreen(
     demoClick: () -> Unit = {},
-    loginClick: (String, String, String) -> Unit = { _, _, _ -> },
+    loginClick: (String, String) -> Unit = { _, _ -> },
 ) {
     AppScreen(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceAround,
     ) {
         var host by rememberSaveable { mutableStateOf("") }
         var username by rememberSaveable { mutableStateOf("") }
-        var password by rememberSaveable { mutableStateOf("") }
         LoginTitle()
         Spacer(modifier = Modifier.height(Dimens.marginDouble))
-        HostField(host) { host = it }
-        Spacer(modifier = Modifier.height(Dimens.marginHalf))
-        TokenField(username) { username = it }
+        Column {
+            HostField(host) { host = it }
+            Spacer(modifier = Modifier.height(Dimens.margin))
+            TokenField(username) { username = it }
+        }
         Spacer(modifier = Modifier.height(Dimens.marginDouble))
-        TextButton(onClick = demoClick) { Text("Demo") }
-        Spacer(modifier = Modifier.height(Dimens.marginHalf))
-        LoginButton(loginClick = { loginClick(host, username, password) })
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextButton(onClick = demoClick) { Text("Demo") }
+            Spacer(modifier = Modifier.height(Dimens.marginHalf))
+            LoginButton(loginClick = { loginClick(host, username) })
+        }
     }
 }
 
@@ -62,6 +71,12 @@ private fun HostField(
         value = host,
         onValueChange = onValueChange,
         label = { Text("Host") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            autoCorrect = false,
+            keyboardType = KeyboardType.Uri,
+            imeAction = ImeAction.Next
+        )
     )
 }
 
@@ -75,6 +90,12 @@ private fun TokenField(
         value = username,
         onValueChange = onValueChange,
         label = { Text("Token") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            autoCorrect = false,
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        )
     )
 }
 
@@ -94,7 +115,11 @@ private fun LoginButton(
 
 @Composable
 private fun LoginTitle() {
-    Text("Login", style = MaterialTheme.typography.headlineLarge)
+    Text(
+        "Login",
+        style = MaterialTheme.typography.headlineLarge,
+        color = MaterialTheme.colorScheme.onBackground
+    )
 }
 
 @Preview(showBackground = true)
